1b3f1d72237bcbc4ba014fc3687c6ceb60dedea1
andr-bulg <andr.bulg24@gmail.com>
2024-04-17 07:19:37 +0300

    lesson_2 homework_6

---
+++ lesson_2/test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from lesson_2.fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    """
+    Функция, которая создаёт и разрушает фикстуру
+    :param request: специальный параметр
+    :return: фикстура (объект класса Application_group)
+    """
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
--- lesson_2/test/test_add_contact.py
+++ lesson_2/test/test_add_contact.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from lesson_2.model.contact import Contact
-from lesson_2.fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    """
-    Функция, которая создаёт и разрушает фикстуру
-    :param request: специальный параметр
-    :return: фикстура (объект класса Application_contact)
-    """
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     """
