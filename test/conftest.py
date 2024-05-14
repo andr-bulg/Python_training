10bcb74cecfcb61679e4105758ccb52061f21dc2
andr-bulg <andr.bulg24@gmail.com>
2024-05-14 10:12:53 +0300

    Конфигурационный файл для описания тестируемого приложения

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from fixture.application import Application
+import json
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
@@ -11,15 +13,14 @@ def app(request):
     :return: фикстура (объект класса Application)
     """
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -37,5 +38,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
 
