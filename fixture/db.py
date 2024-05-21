c4e63ec4d1ca2f7fca02e9ab6903c72d419a8cbf
andr-bulg <andr.bulg24@gmail.com>
2024-05-21 20:01:25 +0300

    Загрузка информации из базы данных в виде объектов

--- fixture/db.py
+++ fixture/db.py
@@ -4,6 +4,8 @@
 """
 
 import pymysql
+from model.group import Group
+
 class DbFixture:
     def __init__(self, host, name, user, password):
         self.host = host
@@ -11,6 +13,19 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host=host, db=name, user=user, password=password)
+
+    def get_group_list(self):
+        list_of_groups = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT group_id, group_name, group_header, group_footer FROM group_list")
+            for row in cursor:
+                (id_group, name, header, footer) = row
+                list_of_groups.append(Group(id_group=str(id_group), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list_of_groups
+
 
     def destroy(self):
         self.connection.close()
