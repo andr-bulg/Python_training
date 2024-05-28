cad7a3826efe0c179b1a36e5c79ab97eb756263c
andr-bulg <andr.bulg24@gmail.com>
2024-05-28 12:43:26 +0300

    2_Добавление проверки в def test_modify_contact_first_name(app):...

--- test/test_modification_contact.py
+++ test/test_modification_contact.py
@@ -1,24 +1,33 @@
 from model.contact import Contact
 import random
 
-def test_modify_contact_first_name(app):
+def test_modify_contact_first_name(app, db):
     """
     Тестовая функция
     :param app: фикстура (объект, который возвращает функция app())
+    :param db: фикстура работы с бд
     """
-    if app.contact.count() == 0:
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(first_name="Irina", last_name="Ivanova", address="Kazan",
                                    home_phone="+74961234565", mobile_phone="+79161234565",
                                    work_phone="+79981237361", fax="+74981234567",
                                    email_1="ivanova_1@test.ru", email_2="ivanova_2@test.ru",
                                    email_3="ivanova_3@test.ru", day="22", month="July", year="2005"))
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
+    m_contact = Contact(first_name="Василий", last_name=old_contacts[0].last_name,
+                      address=old_contacts[0].address, home_phone=old_contacts[0].home_phone,
+                      mobile_phone=old_contacts[0].mobile_phone, work_phone=old_contacts[0].work_phone,
+                      fax=old_contacts[0].fax, email_1=old_contacts[0].email_1,
+                      email_2=old_contacts[0].email_2, email_3=old_contacts[0].email_3,
+                      day=old_contacts[0].day, month=old_contacts[0].month,
+                      year=old_contacts[0].year)
+    m_contact.id_contact = old_contacts[0].id_contact
     contact = Contact(first_name="Василий")
     contact.id_contact = old_contacts[0].id_contact
     app.contact.modify_first_contact(contact)
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[0] = contact
+    old_contacts[0] = m_contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 def test_modify_contact_last_name(app):
