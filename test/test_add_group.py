1d5d0d695a409b35503a0941363ad98d397f1fb1
andr-bulg <andr.bulg24@gmail.com>
2024-06-13 22:16:28 +0300

    Modify /test/test_add_group.py

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+import allure
 
 def test_add_group(app, db, json_groups, check_ui):
     """
@@ -11,12 +11,15 @@ def test_add_group(app, db, json_groups, check_ui):
     :param check_ui: фикстура, отвечающая за проверку UI
     """
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        new_groups = map(app.group.clean, new_groups)
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step("Given a group list"):
+        old_groups = db.get_group_list()
+    with allure.step(f"When I add a group {group} to the list"):
+        app.group.create(group)
+    with allure.step(f"Then the new group list is equal to the old list with the added group"):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            new_groups = map(app.group.clean, new_groups)
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
